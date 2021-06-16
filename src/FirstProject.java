6c1ebd952a32d6f9577c195c63c1ab336c59dc3a
yasinyurek <yasinyurek.dev@gmail.com>
2021-06-16 17:16:03 +0300

    Taksimetre Programı

--- src/FirstProject.java
+++ src/FirstProject.java
@@ -4,7 +4,10 @@ public class FirstProject {
     public static void main(String[] args) 
     {
         Scanner input = new Scanner(System.in);
-        float price;
+
+
+        // -- KDV HESAPLAYAN PROGRAM --//
+        /*float price;
         float kdv;
 
         System.out.println("Ürün Fiyatı Giriniz");
@@ -15,7 +18,34 @@ public class FirstProject {
 
         price = price + kdv;
 
-        System.out.print("Ürünün KDV'li Fiyatı = " + price);
+        System.out.print("Ürünün KDV'li Fiyatı = " + price);*/
+
+        // -- ---------------- --//
+
+        // -- TAKSİMETRE --//
+
+        double distance;
+        double price;
+        double startPrice = 10;
+        double kmPrice = 2.20;
+
+
+        System.out.println("Gidilen Mesafeyi Giriniz");
+
+        distance = input.nextDouble();
+
+        price = ((distance * kmPrice) + startPrice);
+        if (price <= 20)
+            System.out.println(" Ücret = 20 TL");
+        else
+        System.out.print("Ücret :" +price);
+
+
+
+
+
+
+
 
 
 
